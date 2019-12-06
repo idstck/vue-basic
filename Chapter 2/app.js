45cfa67b9a8a2cff03981eb47f64cb2c80d4ba72
idstack <idstackdeveloper@gmail.com>
2019-12-06 17:12:09 +0700

    Perbedaan Method dan Computed

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -3,12 +3,32 @@ var app = new Vue({
     data: {
         slugText: 'The Quick #(*!&@ Brown 29'
     },
-    computed: { 
+    computed: {
+        // now: function () {
+        //     var date = new Date();
+        //     return (
+        //         String(date.getHours()) + 
+        //         String(date.getMinutes()) +
+        //         String(date.getSeconds())
+        //     )
+        // },
+ 
         slugetize: function () {
             return this.slugText
                 .toLowerCase()
                 .replace(/[^\w ]+/g, '')
-                .replace(/ +/g, '-');
+                .replace(/ +/g, '-') + '-' + this.now();
         }
     },
+
+    methods: {
+        now: function () {
+            var date = new Date();
+            return (
+                String(date.getHours()) + 
+                String(date.getMinutes()) +
+                String(date.getSeconds())
+            )
+        },
+    }
 });
