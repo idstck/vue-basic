ae05d3c1913e47630ec28ae9dc4bb5959c4618fc
idstack <idstackdeveloper@gmail.com>
2019-12-06 14:56:45 +0700

    Mengenal Events dan Membuat Method

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         maximum: 50,
-        products: null
+        products: null,
+        cart: []
     },
     mounted: function() {
         fetch('https://hplussport.com/api/products/order/price')
@@ -10,5 +11,10 @@ var app = new Vue({
         .then(data => {
             this.products = data;
         });
+    },
+    methods: {
+        addItem: function (product) {
+            this.cart.push(product);
+        }
     }
 });
