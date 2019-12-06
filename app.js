5c2b9b4765649171cdd28aeebf44c261f67e8a80
idstack <idstackdeveloper@gmail.com>
2019-12-06 11:42:46 +0700

    Mengenal Reactive Data pada Vue

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+var data = {
+    "name": "Bamboo Thermal Ski Coat",
+    "description": "You'll be the most environmentally conscious skier on the slopes - and the most stylish - wearing our fitted bamboo thermal ski coat, made from organic bamboo with recycled plastic down filling.",
+    "price": "99",
+    "image": "https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+}
+
 var app = new Vue({
     el: '#app',
-    data: {
-        name: 'Ski Coat Thermal'
-    }
+    data: data
 });
