04bf2917df350730f3331079d337e3b8b8834520
idstack <idstack@idstacks-MacBook-Pro.local>
2019-12-09 16:00:25 +0700

    Mengatur style menggunakan JavaScript

--- Chapter 3/app.js
+++ Chapter 3/app.js
@@ -23,6 +23,21 @@ var app = new Vue({
         }
     },
     methods: {
+        before: function (el) {
+            el.className = 'd-none'
+        },
+        enter: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animated fadeInRight'
+            }, delay)
+        },
+        leave: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animated fadeOutRight'
+            }, delay)
+        },
         addItem: function(product) {
             this.cart.push(product);
         }
