baf1db3afb99d4c0623cf7d284bc77b5199f5e87
idstack <idstack@idstacks-MacBook-Pro.local>
2019-12-10 18:09:37 +0700

    Membuat Computed Fungsi Total Cart

--- Chapter 4/app.js
+++ Chapter 4/app.js
@@ -25,6 +25,20 @@ var app = new Vue({
     computed: {
         sliderState: function() {
             return this.style.sliderStatus ? 'd-flex' : 'd-none';
+        },
+        cartTotal: function () {
+            let sum = 0;
+            for (key in this.cart) {
+                sum = sum + (this.cart[key].product.price * this.cart[key].qty);
+            }
+            return sum;
+        },
+        cartQty: function () {
+            let qty = 0;
+            for (key in this.cart) {
+                qty = qty + this.cart[key].qty;
+            }
+            return qty;
         }
     },
     methods: {
