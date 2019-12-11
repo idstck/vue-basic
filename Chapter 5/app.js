e985cafaaecd0e8e1a77f4632a1173e629a648b3
idstack <idstack@idstacks-MacBook-Pro.local>
2019-12-11 15:11:59 +0700

    Menggunakan Event pada Parent Component

--- Chapter 5/app.js
+++ Chapter 5/app.js
@@ -40,7 +40,7 @@ Vue.component('product-list', {
         <transition-group name="fade" tag="div" @beforeEnter="before" @enter="enter" @leave="leave">
             <div class="row d-none mb-3 align-items-center" v-for="(item, index) in products" :key="item.id" v-if="item.price <= Number(maximum)" :data-index="index">
                 <div class="col-1 m-auto">
-                    <button class="btn btn-info" v-on:click="addItem(item)">+</button>
+                    <button class="btn btn-info" @click="$emit('add', item)">+</button>
                 </div>
                 <div class="col-sm-4">
                     <img :src="item.image" :alt="item.name" class="img-fluid d-block">
