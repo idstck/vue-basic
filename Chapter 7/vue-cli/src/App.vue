833ceea5aa8e45c73fddaeffb3e17a2973bf3d57
idstack <idstack@idstacks-MacBook-Pro.local>
2019-12-13 10:33:13 +0700

    Membuat Component Price

--- Chapter 7/vue-cli/src/App.vue
+++ Chapter 7/vue-cli/src/App.vue
@@ -3,16 +3,19 @@
     <h1>IDShop</h1>
     <p class="animated fadeInRight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quasi dolores sunt dolorem omnis amet eaque odio! Velit eaque culpa, adipisci molestiae sed nihil, totam quisquam ad ut aspernatur architecto.</p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <price value="4.23"></price>
   </div>
 </template>
 
 <script>
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import Price from './components/Price.vue';
 
 export default {
   name: "app",
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon, 
+    Price
   }
 };
 </script>
---
+++ Chapter 7/vue-cli/src/components/Price.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>{{ this.prefix + Number.parseFloat(this.value).toFixed(this.precision) }}</span>
+</template>
+
+<script>
+export default {
+    props: {
+        value: Number,
+        prefix: {
+            type: String,
+            default: 'Rp'
+        },
+        precision: {
+            type: Number,
+            default: 2
+        }
+    }
+}
+</script>
