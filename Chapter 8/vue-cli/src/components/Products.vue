081dc81b2183934a08e385ed8e941fbe8fdc65e3
IDStack <idstack@outlook.co.id>
2022-08-05 12:25:48 +0700

    fix bug filter price item

--- Chapter 8/vue-cli/src/components/Products.vue
+++ Chapter 8/vue-cli/src/components/Products.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-        <h1>IDShop</h1>
-        <navbar :cart="cart" :cartQty="cartQty" :cartTotal="cartTotal"></navbar>
-        <price-slider :sliderStatus=" sliderStatus"></price-slider>
-        <product-list :maximum="maximum" :products="products"></product-list>
-    </div>
+	<div>
+		<h1>IDShop</h1>
+		<navbar :cart="cart" :cartQty="cartQty" :cartTotal="cartTotal"></navbar>
+		<price-slider :sliderStatus="sliderStatus"></price-slider>
+		<pre>
+            <!-- <div>{{ JSON.stringify(products, null, 2) }}</div> -->
+        </pre>
+		<product-list :maximum="maximum" :products="products"></product-list>
+	</div>
 </template>
 
 <script>
@@ -13,20 +16,20 @@ import PriceSlider from './PriceSlider.vue';
 import Navbar from './Navbar.vue';
 
 export default {
-    name: "products",
-    props: [
-        "products", 
-        "maximum", 
-        "cart", 
-        "cartQty", 
-        "cartTotal", 
-        "sliderStatus", 
-        "sliderState"
-    ],
-    components: {
-        Navbar,
-        PriceSlider,
-        ProductList,
-    }
-}
+	name: 'products',
+	props: [
+		'products',
+		'maximum',
+		'cart',
+		'cartQty',
+		'cartTotal',
+		'sliderStatus',
+		'sliderState',
+	],
+	components: {
+		Navbar,
+		PriceSlider,
+		ProductList,
+	},
+};
 </script>
